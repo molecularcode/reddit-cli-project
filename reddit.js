10bf01207123ca79e8295c50740fd966b89697a1
Alex Ruaux <alex@molecularcodewebdesign.com>
2016-04-20 20:44:04 +0000

    Basic feature: the main menu - add main menu using npm inquirer

--- reddit.js
+++ reddit.js
@@ -12,3 +12,19 @@ var util = require('util');
 --------------------------------------------------------------------------------------- */
 //  When the user chooses the homepage option, you should display the list of posts from the getHomepage function you created previously. For each post, list at least some of the info that appears on reddit: title, url, votes, username. After the list of posts is displayed, you should display the main menu again.
 
+var menuChoices = [
+  {name: 'Show reddit homepage', value: 'HOMEPAGE'},
+  {name: 'Show subreddit', value: 'SUBREDDIT'},
+  {name: 'List subreddits', value: 'SUBREDDITS'}
+];
+
+inquirer.prompt({
+  type: 'list',
+  name: 'menu',
+  message: 'What do you want to browse?',
+  choices: menuChoices
+}).then(
+  function(answers) {
+    console.log(answers);
+  }
+);
