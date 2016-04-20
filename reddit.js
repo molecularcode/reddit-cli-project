1f909237010c5d26066d8d4729e8b4bba628027c
Alex Ruaux <alex@molecularcodewebdesign.com>
2016-04-20 20:34:19 +0000

    import reddit module to main reddit.js file

---
+++ reddit.js
@@ -0,0 +1,14 @@
+// import my reddit module file
+var reddit = require("./lib/reddit.js");
+
+// > npm install inquirer, colors, util
+var inquirer = require('inquirer');
+var colors = require('colors');
+var util = require('util');
+
+//console.log(util.inspect(reddit, { showHidden: true, depth: 3, colors: true }));
+
+/* Basic feature: the main menu
+--------------------------------------------------------------------------------------- */
+//  When the user chooses the homepage option, you should display the list of posts from the getHomepage function you created previously. For each post, list at least some of the info that appears on reddit: title, url, votes, username. After the list of posts is displayed, you should display the main menu again.
+
