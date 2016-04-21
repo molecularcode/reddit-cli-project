5627591aa7d95a60ea44c46a5cbf4bcb64062f55
Alex Ruaux <alex@molecularcodewebdesign.com>
2016-04-21 01:20:46 +0000

    Basic feature: the main menu - format output, add emojis, calculate time submitted

--- reddit.js
+++ reddit.js
@@ -4,7 +4,11 @@ var reddit = require("./lib/reddit.js");
 // > npm install inquirer, colors, util
 var inquirer = require('inquirer');
 var colors = require('colors');
+var emoji = require('node-emoji');
 var util = require('util');
+var currentTime = Math.floor(new Date().getTime() / 1000); // in seconds
+var up = emoji.get('thumbsup').green;
+var dwn = emoji.get('thumbsdown').red;
 
 //console.log(util.inspect(reddit, { showHidden: true, depth: 3, colors: true }));
 
@@ -13,18 +17,67 @@ var util = require('util');
 //  When the user chooses the homepage option, you should display the list of posts from the getHomepage function you created previously. For each post, list at least some of the info that appears on reddit: title, url, votes, username. After the list of posts is displayed, you should display the main menu again.
 
 var menuChoices = [
-  {name: 'Show reddit homepage', value: 'HOMEPAGE'},
-  {name: 'Show subreddit', value: 'SUBREDDIT'},
-  {name: 'List subreddits', value: 'SUBREDDITS'}
+    {
+        name: 'Show reddit homepage',
+        value: 'HOMEPAGE'
+    },
+    {name: 'Show subreddit', value: 'SUBREDDIT'},
+    {name: 'List subreddits', value: 'SUBREDDITS'},
 ];
 
-inquirer.prompt({
-  type: 'list',
-  name: 'menu',
-  message: 'What do you want to browse?',
-  choices: menuChoices
-}).then(
-  function(answers) {
-    console.log(answers);
-  }
-);
+function redditRun() {
+    inquirer.prompt({
+      type: 'list',
+      name: 'menu',
+      message: 'What do you want to browse?',
+      choices: menuChoices
+    }).then(function(answers) {
+            if (answers.menu === 'HOMEPAGE') {
+                reddit.getHomepage(function(result){
+                    var hpObj = {};
+                    var posted;
+                    result.data.children.forEach(function(x, index) {
+                        // build homepage object
+                        hpObj[result.data.children[index].data.id] = {
+                            title:result.data.children[index].data.title,
+                            url:result.data.children[index].data.url,
+                            ups:result.data.children[index].data.ups,
+                            downs:result.data.children[index].data.downs,
+                            author:result.data.children[index].data.author,
+                            created:result.data.children[index].data.created_utc
+                        };
+                    });
+    
+                    function postedTime(postedTime, currentTime) {
+                        var postedMins = Math.floor((currentTime - postedTime) / 60);
+                        //posted = postedMins;
+                        //console.log(postedTime, currentTime, posted);
+                        if (postedMins < 60) {
+                            posted = Math.round(postedMins) + " minutes ago";
+                        }
+                        else if (postedMins > 60 && postedMins < 1440) {
+                        var postedHrs = Math.round(postedMins / 60);
+                            posted = (postedHrs + " hours ago");
+                        }
+                        else {
+                            var postedDays = Math.round(postedMins / 60 / 24);
+                            posted = (postedDays + " days ago");
+                        }
+                    }
+    
+                    for(var prop in hpObj) {
+                        postedTime(hpObj[prop].created, currentTime);
+                        console.log(hpObj[prop].title.bold + "\n" + hpObj[prop].url.blue + "\nsubmitted: ".red + posted + " | ".red + up + hpObj[prop].ups + " | ".red + dwn + hpObj[prop].downs + " | author: ".red + hpObj[prop].author + "\n");
+                    }
+                    console.log(redditRun());
+                });
+            }
+            else {
+                console.log(answers.menu);
+                console.log(redditRun());
+            }
+        }
+    );
+}
+
+redditRun();
